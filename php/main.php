4d93b0cc703094f18fdfaed79eb0c10d839c1a1a
PierreElef <pierre.elefterion@ynov.com>
2018-04-06 10:33:10 +0200

    MAJ-0604

--- php/main.php
+++ php/main.php
@@ -1,3 +1,15 @@
-//Début de la partie
-//Début de la partie
+//Début du jeu
+//Choisir du nombre de joueurs réels
+//Création joueurs réels
+//Création joueurs IA
+
+//Création du plateau
+    //Création des cases
+        //Création des rues
+        //Création des cases Chances
+        //
+//Création des dés
+//Création des cartes
+//Création des pions
 //
+//Début de la partie
