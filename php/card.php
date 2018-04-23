ebe1084993159ed5a5c0b15570c12311c101b6a4
Hugo Schieres <hugo.schieres@ynov.com>
2018-04-23 09:26:08 +0200

    maj card

--- php/card.php
+++ php/card.php
@@ -1,5 +1,31 @@
 <?php
+include ('Player.php');
+class Card{
+private $ID;
+private $message;
+private $type;
+takeMoneyfromPlayer()
+{
 
+}
+giveMoneyfromPlayer()
+{
+
+}
+sendPlayertoJail()
+{
+
+}
+changePositionPlayer()
+{
+	
+}
+leaveJail()
+{
+	$jailStatus = 1;
+}
+
+}
 
 
 
